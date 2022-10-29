70564084ba93120126ea6e33699185ac766ee63f
nbogie <neillbogie@googlemail.com>
2022-10-30 00:29:08 +0100

    remove unused utils

--- src/utils.ts
+++
@@ -1,9 +0,0 @@
-export function assert(condition: unknown, msg?: string): asserts condition {
-    if (condition === false) throw new Error(msg);
-}
-
-export class UnreachableCodeError extends Error {
-    constructor(myNever: never, message: string) {
-        super(message);
-    }
-}
